c03b30dc56a6cab6a75896f5ad895760ced89524
Quin-Riley-A <quin.asselin1@gmail.com>
2023-01-03 21:13:06 -0500

    build roadmap of client controller and salon context class

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+
+    }
+
+    public ActionResult Create()
+    {
+      
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+
+    }
+
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
+
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Clients.Update(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+  }
+}
